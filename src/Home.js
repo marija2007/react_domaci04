563cbee83388cd1b64d462f1fec661793851bcac
marija20 <marija.bujisic@amplitudo.me>
2020-02-28 14:49:17 +0100

    commit

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React, { Component, useState, useEffect } from 'react';
+import FullScreenDialog from "./FullScreenDialog";
+
+
+
+class Home extends Component {
+/*     constructor(props) {
+        super(props);
+        this.state = {  }
+    } */
+    render() { 
+        return ( 
+            <div>
+                <FullScreenDialog/>
+            </div>
+         );
+    }
+}
+ 
+export default Home;
---
+++ src/IDCard.js
@@ -0,0 +1,38 @@
+import React , { useState , useEffect} from 'react';
+
+
+function IDCard () {
+  /*   const [name, setDatas] = useState('Marija');
+    const [lastName, setDatas] = useState('Bujisic');
+    const[age,setDatas] = useState(23); */
+
+    const [ data, setDatas] = useState([]);
+
+    useEffect(() => {
+          setDatas([
+            ...data,
+                {
+            name: 'Marija',
+            lastName: 'Bujisic',
+            age: 23
+                }
+            ]);
+          }, []); 
+
+    return(
+      <div>
+        {
+          // console.log(data)
+          data.map(item =>{
+            return <div>
+              <h1>{item.name} {item.lastName}</h1>
+              <h5>{item.age}</h5>
+              </div>
+          })
+        }
+      </div>
+    )
+
+}
+
+export default IDCard;
